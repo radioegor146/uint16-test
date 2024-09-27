d7f3705a21376af731cd79d26a22d23ceb3b40b5
Egor Koleda <radioegor146@ctf.by>
2024-09-27 22:09:05 +0300

    test

--- main.cpp
+++ main.cpp
@@ -13,14 +13,10 @@ static const uint32_t __NMH_M1_V[32] = {
     __NMH_M1, __NMH_M1, __NMH_M1, __NMH_M1,
 };
 
-bool nmhash32_broken() {
-
-const char entropy[] =
-    "rwgk8M1uxM6XX6c3teQX2yaw8FQWArmcWUSBJ8dcQQJWHYC9Wt2BmpvETxwhYcJTheTbjf49"
-    "SVRaDJhbEZCq7ki1D6KxpKQSjgwqsiHGSgHLxvPG5kcRnBhjJ1YC8kuh";
+uint32_t nmhash32_broken(volatile char* entropy) {
 
   uint32_t accX[32];
-  memcpy(accX, entropy, sizeof(accX));
+  memcpy(accX, (void*)entropy, sizeof(accX));
 
   size_t i;
 
@@ -28,17 +24,25 @@ const char entropy[] =
     ((uint16_t*)accX)[i] *= ((uint16_t*)__NMH_M1_V)[i];
   }
 
+  for (i = 0; i < 32; ++i) {
+    printf("%04x ", accX[i]);
+  }
+  printf("\n");
+
   uint32_t acc = 0;
   for (i = 0; i < 32; ++i)
     acc += accX[i];
 
   printf("%04x\n", acc);
 
-  return (acc) != UINT32_C(0x249abaee);
+  return (acc);
 }
 
+const char entropy[] =
+    "rwgk8M1uxM6XX6c3teQX2yaw8FQWArmcWUSBJ8dcQQJWHYC9Wt2BmpvETxwhYcJTheTbjf49"
+    "SVRaDJhbEZCq7ki1D6KxpKQSjgwqsiHGSgHLxvPG5kcRnBhjJ1YC8kuh";
 
 int main(int argc, const char** argv) {
-  printf("%d\n", nmhash32_broken());
+  printf("%d\n", nmhash32_broken((volatile char*)entropy) != UINT32_C(0x249abaee));
   return 0;
 }
